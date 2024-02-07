079a7a6404340d987ef5a91e6751ae7e3b4280a1
boysimon10 <simondiouf2710@gmail.com>
2024-02-07 19:23:59 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Import des composants GoogleMap et LoadScript depuis '@react-google-maps/api'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+// Style du conteneur de la carte
+const containerStyle = {
+  width: '800px',
+  height: '600px',
+};
+
+// Emplacement de GOMYCODE POINT E avec La latitude et La longitude
+const center = {
+  lat: 14.696906, // Latitude de GOMYCODE POINT E
+  lng: -17.456929, // Longitude de GOMYCODE POINT E
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Conteneur principal avec une marge en haut
+    <div className="container mt-5">
+      {/* Titre centré */}
+      <h1 className="text-center mb-4">GOMYCODE POINT E</h1>
+      {/* Composant LoadScript pour charger l'API Google Maps avec la clé d'API */}
+      <LoadScript googleMapsApiKey="UN_API_KEY">
+        {/* Composant GoogleMap qui affiche la carte */}
+        <GoogleMap
+          mapContainerStyle={containerStyle} // Style du conteneur de la carte
+          center={center} // Centre de la carte, à GOMYCODE POINT E
+          zoom={10} // Niveau de zoom initial de la carte
+        ></GoogleMap>
+      </LoadScript>
     </div>
   );
 }
-
 export default App;
