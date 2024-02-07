8661cf95c689bee56f22b268c54489640602b42b
boysimon10 <simondiouf2710@gmail.com>
2024-02-07 19:54:50 +0000

    final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-// Import des composants GoogleMap et LoadScript depuis '@react-google-maps/api'
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
 // Style du conteneur de la carte
 const containerStyle = {
-  width: '800px',
-  height: '600px',
+  width: '600px',
+  height: '500px',
 };
 
-// Emplacement de GOMYCODE POINT E avec La latitude et La longitude
+// Emplacement de GOMYCODE POINT E avec la latitude et la longitude
 const center = {
   lat: 14.696906, // Latitude de GOMYCODE POINT E
   lng: -17.456929, // Longitude de GOMYCODE POINT E
@@ -16,20 +17,27 @@ const center = {
 
 function App() {
   return (
-    // Conteneur principal avec une marge en haut
+    <>
+    <Header/>
     <div className="container mt-5">
-      {/* Titre centré */}
-      <h1 className="text-center mb-4">GOMYCODE POINT E</h1>
-      {/* Composant LoadScript pour charger l'API Google Maps avec la clé d'API */}
-      <LoadScript googleMapsApiKey="UN_API_KEY">
-        {/* Composant GoogleMap qui affiche la carte */}
-        <GoogleMap
-          mapContainerStyle={containerStyle} // Style du conteneur de la carte
-          center={center} // Centre de la carte, à GOMYCODE POINT E
-          zoom={10} // Niveau de zoom initial de la carte
-        ></GoogleMap>
-      </LoadScript>
+      <div className="row">
+        <Contact />
+        {/* Colonne pour la carte Google Maps */}
+        <div className="col-md-6">
+          {/* Composant LoadScript pour charger l'API Google Maps avec la clé d'API */}
+          <LoadScript googleMapsApiKey="UN_API_KEY">
+            {/* Composant GoogleMap qui affiche la carte */}
+            <GoogleMap
+              mapContainerStyle={containerStyle} // Style du conteneur de la carte
+              center={center} // Centre de la carte, à GOMYCODE POINT E
+              zoom={10} // Niveau de zoom initial de la carte
+            ></GoogleMap>
+          </LoadScript>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
+
 export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Contact() {
+    return (
+        <div className="col-md-6">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">Name</label>
+              <input type="text" className="form-control" id="name" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Email</label>
+              <input type="email" className="form-control" id="email" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="message" className="form-label">Message</label>
+              <textarea className="form-control" id="message" rows="5"></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+    );
+}
+export default Contact;
